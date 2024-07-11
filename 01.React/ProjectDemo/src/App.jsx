a891129d1b25fcedd269b3707f1df78540b77f7d
Anubisa97 <assamsias97@gmail.com>
2024-07-11 18:31:29 +0300

    SoftUni exercises

---
+++ 01.React/ProjectDemo/src/App.jsx
@@ -0,0 +1,35 @@
+import About from "./components/About";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Info from "./components/Info";
+import Managed from "./components/Managed";
+import Pricing from "./components/Pricing";
+import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Services />
+
+      <About />
+
+      <Managed />
+
+      <Pricing />
+
+      <Testimonial />
+
+      <ContactUs />
+
+      <Info />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
